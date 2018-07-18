2f55a3bbdec75b4ab0986b7d4d02e9713fb8feba
Kuba <kuba0506@gmail.com>
2018-07-18 13:19:04 +0200

    Add config bundle

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,27 @@
 const passport = require('passport');
+const config = require('../config/');
 
 module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email'] // what data we're asking google for
-    }));
-    
+    }), (req, res) => {
+        console.log('/auth/google');
+    }
+    );
+
     // code from google is available
-    app.get('/auth/google/callback', passport.authenticate('google'));
-    
+    app.get('/auth/google/callback', passport.authenticate('google'),
+        (req, res) => {
+            return res.redirect(`${config.host}/surveys`);
+            // return res.redirect('/surveys');
+        });
+
     app.get('/api/logout', (req, res) => {
+        console.log('log out');
         // req.logout() , fn attached by passport, removes cookies
         req.logout();
-        return res.send(req.user);
+        if (!req.user) return res.redirect(`${config.host}/`);
+        // if (!req.user) return res.redirect('/');
     });
 
     // test authentication
--- services/passport.service.js
+++ services/passport.service.js
@@ -1,22 +1,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/dev');
+const keys = require('../config/');
 const mongoose = require('mongoose');
 
 const UserModel = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
     // user (model instance) is passed from googlestrategy callback done fn
-    done(null, user.id); // id from mongo record, this approach is good when we use many providers
+    return done(null, user.id); // id from mongo record, this approach is good when we use many providers
 });
 
 passport.deserializeUser(async (userId, done) => {
     // search for user in collection
     try {
         const user = await UserModel.findById(userId);
-        done(null, user)
+        return done(null, user);
     } catch (e) {
-        done(e, null);
+        return done(e, null);
     }
 });
 
@@ -26,19 +26,18 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, async (accessToken, refreshToken, profile, done) => {
-        
         try {
             // saving profile.id to authenticate in future
             const existingUser = await UserModel.findOne({ googleID: profile.id });
-    
+
             if (!existingUser) {
                 const newUser = await new UserModel({ googleID: profile.id }).save();
-                done(null, newUser);
-            } else {
-                done(null, existingUser);
+                return done(null, newUser);
             }
-        } catch(e) {
-            done(e, null);
+
+            return done(null, existingUser);
+        } catch (e) {
+            return done(e, null);
         }
     }
     ));
